3b9f8f5a0b42301cb0631d8e341cf3be845d083b
Felipe Ribeiro <felipeonlinebr@gmail.com>
2023-02-18 11:39:35 -0300

    Primeiros stilos e dado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import Dice from "./components/Dice";
+
 export default function App() {
   return (
-    <div className="App">
-      
+    <div className="Game">
+      <div className="game-box">
+        <div className="dices">
+          <Dice value="1" />
+          <Dice value="1" />
+          <Dice value="1" />
+          <Dice value="1" />
+          <Dice value="1" />
+          <Dice value="1" />
+          <Dice value="1" />
+          <Dice value="1" />
+          <Dice value="1" />
+          <Dice value="1" />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Dice.jsx
@@ -0,0 +1,7 @@
+export default function Dice({ value }) {
+    return (
+        <div className="dice">
+            <h2>{value}</h2>
+        </div>
+    );
+}
