8feeb48cf674dc55816cd7b13926b42b08d078c4
Felipe Ribeiro <felipeonlinebr@gmail.com>
2023-02-18 11:50:50 -0300

    Layout e estilização geral terminados

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ export default function App() {
   return (
     <div className="Game">
       <div className="game-box">
+        <div className="top-texts">
+          <h1>Tenzies</h1>
+          <p>Role até que todos os dados sejam iguais. Clique em cada dado para congelá-lo em seu valor atual entre os lançamentos.</p>
+        </div>
         <div className="dices">
           <Dice value="1" />
           <Dice value="1" />
@@ -16,6 +20,7 @@ export default function App() {
           <Dice value="1" />
           <Dice value="1" />
         </div>
+        <button className="roll-button">Rolar</button>
       </div>
     </div>
   );
