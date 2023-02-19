c8bea3f2db800a1298de18f80598a4da2cb85f1a
Felipe Ribeiro <felipeonlinebr@gmail.com>
2023-02-19 11:01:37 -0300

    Publicação do app para github pages

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,7 +1,17 @@
 export default function Dice({ onClick, value, isHeld }) {
+
+    
     return (
-        <div onClick={onClick} className={isHeld ? "dice dice-held" : "dice"}>
-            <h2>{value}</h2>
+        <div onClick={onClick} className={isHeld ? `dice dice-${value} dice-held` : `dice dice-${value}`}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
         </div>
     );
 }
